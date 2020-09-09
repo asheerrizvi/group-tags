fd20916c782c39fc4de224005be0e67964878785
asheerrizvi <asheer.vil@gmail.com>
2020-09-09 19:59:15 +0530

    Updates

---
+++ precache-manifest.cea18bb32cecb9f5b85bc95a6103e8fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4058c8c86c3aeba79989926bffe919",
+    "url": "/group-tags/index.html"
+  },
+  {
+    "revision": "be1e93cd4d7f407841f8",
+    "url": "/group-tags/static/css/2.3107b260.chunk.css"
+  },
+  {
+    "revision": "be1e93cd4d7f407841f8",
+    "url": "/group-tags/static/js/2.b5fe4cf4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/group-tags/static/js/2.b5fe4cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b005bd29b9c89fd5cb",
+    "url": "/group-tags/static/js/main.b1e16645.chunk.js"
+  },
+  {
+    "revision": "af4ac0b91d7dfc12a6d3",
+    "url": "/group-tags/static/js/runtime-main.5e633629.js"
+  }
+]);
